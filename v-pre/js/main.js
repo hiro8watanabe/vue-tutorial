d894446f276a686bf113f8bf3fa089ca9dfd9cd3
hiro <onecast.onelost615@gmail.com>
2023-06-16 17:04:52 +0700

    v-preディレクティブ基礎

---
+++ v-pre/js/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data: () => ({
+    message: "Hello Vue.js!",
+  }),
+  methods: {
+    clickHandler: function (e) {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+});
+app.mount("#app");
